447f5c96b1e3a7723bfc768c18b0b966d538605d
Arielle Isaacs <amisaacs@g.hmc.edu>
2019-09-25 23:16:40 -0700

    Have webpage with nav bar

---
+++ Toober/src/Components/HomePage.js
@@ -0,0 +1,24 @@
+// import React, { Component } from 'react';
+// class HomePage extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Home page
+//       </div>
+//     )
+//   }
+// }
+// export default HomePage;
+
+import React from 'react';
+ 
+const home = () => {
+    return (
+       <div>
+          <h1>Home</h1>
+           <p>Home page body content</p>
+       </div>
+    );
+}
+ 
+export default home;

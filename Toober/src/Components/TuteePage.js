8946dbbb64a01424c0a382dbbe7c0e62f0b4505e
Arielle Isaacs <amisaacs@g.hmc.edu>
2019-09-28 11:27:21 -0700

    working on adding a drop down menu

--- Toober/src/Components/TuteePage.js
+++ Toober/src/Components/TuteePage.js
@@ -5,13 +5,16 @@ import firebase from '../FirebaseConfig.js';
 import Theme from './Theme.js';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
+import { FormControl, Input, InputLabel, FormHelperText } from '@material-ui/core';
+
 
 class Tutee extends Component {
     constructor() {
         super();
         this.state = {
           username: '',
-          problem: ''
+          problem: '',
+          subject: ''
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -28,14 +31,16 @@ class Tutee extends Component {
     const itemsRef = firebase.database().ref('/problems/');
     const item = {
       user: this.state.username,
-      problem: this.state.problem
+      problem: this.state.problem,
+      subject: this.state.subject
     }
     itemsRef.push(item).catch(function(error) {
       console.error("Error saving message to Database:", error);
     });
     this.setState({
       username: '',
-      problem: ''
+      problem: '',
+      subject: ''
     })
   }
     
@@ -46,6 +51,13 @@ class Tutee extends Component {
           <h1>Tutee</h1>
         <form onSubmit={this.handleSubmit}>
             <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username}/>
+            <select id="lang" onChange={this.handleChange} value={this.state.subject}>
+                  <option value="Math">Math</option>
+                  <option value="Biology">Biology</option>
+                  <option value="English">English</option>
+               </select>
+               <p></p>
+               <p>{this.state.subject}</p>
             <input type="text" name="problem" placeholder="What is the problem you are working on?" onChange={this.handleChange} value={this.state.problem}/>
         <Button variant="contained" color="secondary">
           Add Question

3064901152adf47551f62c0bf51791e60e6eee53
Arielle Isaacs <amisaacs@g.hmc.edu>
2019-09-29 10:19:21 -0700

    Added a dropdown to choose the subject for the tutor

--- Toober/src/Components/TutorPage.js
+++ Toober/src/Components/TutorPage.js
@@ -6,8 +6,10 @@ class Tutor extends Component {
         super();
         // We will populate this list with data from Firebase
         this.state = {
-          problems: []
+          problems: [],
+          subject: 'Math'
         }
+        this.handleChange = this.handleChange.bind(this);
     }
 
 componentDidMount() {
@@ -33,24 +35,37 @@ componentDidMount() {
         });
     });
 }
+
+handleChange(e) {
+    // Update the state when necessary
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
  
 render(){
     return (
         <div>
            <h1>Tutor</h1>
-            <p>tutor page</p>
             <h2> Problem Sets</h2>
+            <select id="lang" name="subject" onChange={this.handleChange} value={this.state.subject}>
+                <option value="Math">Math</option>
+                <option value="Biology">Biology</option>
+                <option value="English">English</option>
+            </select>
             {this.state.problems.map((problem) => {
+                if (problem.subject === this.state.subject) {
                 return (
                     <div key={problem.id}>
                         <div>
                             <div>
-                                <h3>{problem.subject} : {problem.problem}</h3>
+                                <h3>{problem.problem}</h3>
                                 <p>{problem.username}</p>
                             </div>
                         </div>
                     </div>
-                )
+                )} 
+                return null
             })}
         </div>
      );

27b25090c3ef5ff6ccc3c7cba72bc169de612df0
Arielle Isaacs <amisaacs@g.hmc.edu>
2019-10-05 10:45:38 -0700

    pointing to a different database for chat testing

--- Toober/src/FirebaseConfig.js
+++ Toober/src/FirebaseConfig.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
-  authDomain: "fir-tutorialchat-70cc4.firebaseapp.com",
-  databaseURL: "https://fir-tutorialchat-70cc4.firebaseio.com",
-  projectId: "fir-tutorialchat-70cc4",
-  storageBucket: "fir-tutorialchat-70cc4.appspot.com",
-  messagingSenderId: "441844295635",
-  appId: "1:441844295635:web:1024895ac820b72a727126"
+  authDomain: "chatpoc-ad7c2.firebaseapp.com",
+  databaseURL: "https://chatpoc-ad7c2.firebaseio.com",
+  projectId: "chatpoc-ad7c2",
+  storageBucket: "chatpoc-ad7c2.appspot.com",
+  messagingSenderId: "956577952304",
+  appId: "1:956577952304:web:41445bad53598406d4f3f5"
 };
 
 firebase.initializeApp(firebaseConfig);

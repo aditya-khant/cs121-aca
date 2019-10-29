32d120e961381900841790e1f33dc640ff3a8970
Aditya Khant <33049089+aditya-khant@users.noreply.github.com>
2019-10-29 15:03:47 -0700

    Context based Nav Bar added

--- Toober/src/Components/Navigation.js
+++ Toober/src/Components/Navigation.js
@@ -1,8 +1,5 @@
 import React, { useContext } from "react";
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Tab from "@material-ui/core/Tab";
+import {Button, Toolbar, Typography, AppBar} from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 import Theme from './Theme.js';
@@ -11,39 +8,55 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { AuthContext } from "../Auth";
 import app from "../FirebaseConfig";
 
+import {Home, School, LockOpen, Lock, AccountCircle} from '@material-ui/icons';
+import {Teach} from 'mdi-material-ui';
+
 const Navigation = () => {
     const { currentUser } = useContext(AuthContext);
     var login = "";
     var loginText = ""
+    
     if(currentUser)
     {
-      login = "/Login";
-      loginText = "Logout";
+      return (
+        <div>
+         <MuiThemeProvider theme={Theme}>
+           <AppBar position="static" color = 'primary' /*Material UI navigation bar look*/>
+              <Toolbar>
+                 <Typography  color = 'secondary'>
+                 <Button startIcon={<Home />} color ="inherit" size="large" component={Link} to="/" underline="none" >Home</Button>
+                  <Button startIcon={<Teach />} color ="inherit" size="large" underline="none" component={Link} to="/Tutor" >Tutor</Button>
+                  <Button startIcon={<School />} color ="inherit" size="large" underline="none" component={Link} to="/Tutee" >Tutee</Button>
+                   <Button startIcon={<Lock />}color ="inherit" size="large" underline="none" component={Link} onClick={() => app.auth().signOut()}>Logout</Button>
+                   <Button startIcon={<AccountCircle />} color="inherit" >{"Welcome "+ app.auth().currentUser.email} </Button>     
+                 </Typography>
+              </Toolbar>
+            </AppBar>
+           </MuiThemeProvider>
+         </div>
+        )
     }
     else
     {
-      login = "/Login";
-      loginText = "Login";
-    }
-    return (
+      return (
       <div>
        <MuiThemeProvider theme={Theme}>
          <AppBar position="static" color = 'primary' /*Material UI navigation bar look*/>
             <Toolbar>
                <Typography  color = 'secondary'>
-                  <Tab label="Home" component={Link} to="/" /*Link and to are how we integrate with react-router*//>
-                  <Tab label="Tutor" component={Link} to="/Tutor" /*We set up the "to" suffix in App.js*//>
-                  <Tab label="Tutee" component={Link} to="/Tutee" />
-                  <Tab label={loginText} component={Link} to={login} />
-                  <Tab label="Sign Up" component={Link} to="/Signup" />
-                  <button onClick={() => app.auth().signOut()}>Sign out</button>
-                  
+               <Button startIcon={<Home />} color ="inherit" size="large" component={Link} to="/" underline="none" >Home</Button>
+                <Button startIcon={<Teach />} color ="inherit" size="large" underline="none" component={Link} to="/Tutor" >Tutor</Button>
+                <Button startIcon={<School />} color ="inherit" size="large" underline="none" component={Link} to="/Tutee" >Tutee</Button>
+                <Button startIcon={<LockOpen />}color ="inherit" size="large" underline="none" component={Link} to="/Login">Login</Button>
+                <Button startIcon={<AccountCircle />} color ="inherit" size="large" underline="none" component={Link} to="/Signup" >Signup</Button>
                </Typography>
             </Toolbar>
           </AppBar>
          </MuiThemeProvider>
        </div>
-    );
+      )
+    }
+
 }
  
 export default Navigation;

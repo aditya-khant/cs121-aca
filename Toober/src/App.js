b8422e81ceec05f30636215dbd2db43e26f2e66c
Arielle Isaacs <amisaacs@g.hmc.edu>
2019-11-13 12:58:23 -0800

    can only access profile if logged in

--- Toober/src/App.js
+++ Toober/src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
               <PrivateRoute exact path="/tutee" component={Tutee}/>
               <Route exact path="/login" component={Login}/>
               <Route exact path="/signup" component={SignUp}/>
-              <Route exact path="/profile" component={Profile}/>
+              <PrivateRoute exact path="/profile" component={Profile}/>
             </Switch>
           </div> 
         </Router>

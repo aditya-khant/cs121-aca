835a0e1ee3b408c8ed2a87469d43b177245344d9
Arielle Isaacs <amisaacs@g.hmc.edu>
2019-11-12 23:24:33 -0800

    working on getting information displayed on profile

--- Toober/src/Components/Profile.js
+++ Toober/src/Components/Profile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import firebase from '../FirebaseConfig.js';
+import {Grid} from "@material-ui/core"
+
 
 class Profile extends Component {
     constructor() {
@@ -20,13 +22,40 @@ class Profile extends Component {
             this.state.photoUrl = user.photoURL;
         }
 
-    }
+    console.log(user.displayName);
+    console.log(user.email);
+    console.log(user.photoURL);
+
+    }   
 
     render() {
+        var user = firebase.auth().currentUser;
+        let header;
+        if (user != null) {
+            const userName = user.displayName;
+            const imageURL = user.photoURL;
+            if (imageURL !== ""){
+                header = (
+                  <div>
+                    <h1>{userName}</h1>
+                    <img src={imageURL} alt = "the user" width="100%" />
+                  </div>
+                )
+              } else {
+                header = (
+                  <div>
+                    <h1>{userName}</h1>
+                  </div>
+                )
+              }
+        }
         return (
-        <div>Hello</div>
+        <div>
+            <Grid item xs={3}>
+          {header}
+        </Grid></div>
         )
-    }
+}
 }
 
 export default Profile;

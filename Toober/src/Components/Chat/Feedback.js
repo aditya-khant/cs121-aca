c1b78f61af7e8dbca4529e11402588f05b3c113f
Arielle Isaacs <amisaacs@g.hmc.edu>
2019-11-15 10:47:48 -0800

    working on removing the chat database information when necessary

--- Toober/src/Components/Chat/Feedback.js
+++ Toober/src/Components/Chat/Feedback.js
@@ -15,8 +15,8 @@ class Feedback extends Component {
 
         this.handleClickOpen = this.handleClickOpen.bind(this);
         this.handleClose = this.handleClose.bind(this);
-        this.closeQuestionAndChat = this.closeQuestionAndChat.bind(this);
-        this.closeChat = this.closeChat.bind(this);
+        // this.closeQuestionAndChat = this.closeQuestionAndChat.bind(this);
+        // this.closeChat = this.closeChat.bind(this);
     }
 
     handleClickOpen(){
@@ -32,18 +32,17 @@ class Feedback extends Component {
       };
 
 
-      closeQuestionAndChat() {
-        this.closeChat();
-        // we can successfully remove the problem. this next line works
-        firebase.database().ref('problems/' + this.state.problem).remove();
-      }
+    //   closeQuestionAndChat() {
+    //     this.closeChat();
+    //     // we can successfully remove the problem
+    //     firebase.database().ref('problems/' + this.state.problem).remove();
+    //   }
 
-      closeChat() {
-        // broken, automatically remakes
-        // we also have to figure out how to redirect the user
-        let ref = firebase.database().ref('chat/' + this.state.tableTitle);
-        ref.remove();
-      }
+    //   closeChat() {
+    //     // broken, automatically remakes
+    //     let ref = firebase.database().ref('chat/' + this.state.tableTitle);
+    //     ref.remove();
+    //   }
 
     render(){
     return(
@@ -51,9 +50,13 @@ class Feedback extends Component {
             <Dialog  open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
             <DialogTitle id="form-dialog-title">Exit</DialogTitle>
             <DialogContent>
-                    <Link to="/"><Button onClick = {this.closeQuestionAndChat} variant="outlined" color="primary">My question was answered</Button></Link>
+                    {/* <Link to="/" closeChat = {true} closeQuestion = {true}><Button onClick = {this.closeQuestionAndChat} variant="outlined" color="primary">My question was answered</Button></Link>
                     <Link to="/"><Button variant="outlined" color="primary">My question was not answered and I want to keep talking to this tutor</Button></Link>
-                    <Link to="/"><Button onClick = {this.closeChat} variant="outlined" color="primary">My question was not answered but I want to talk to a new tutor</Button></Link>
+                    <Link to="/" closeChat = {true} closeQuestion= {false}><Button onClick = {this.closeChat} variant="outlined" color="primary">My question was not answered but I want to talk to a new tutor</Button></Link> */}
+
+                    <Link to= {{ pathname: '/', query: {closeChat: true, closeQuestion: true, problem: this.state.problem, tableTitle: this.state.tableTitle}}}><Button variant="outlined" color="primary">My question was answered</Button></Link>
+                    <Link to="/"><Button variant="outlined" color="primary">My question was not answered and I want to keep talking to this tutor</Button></Link>
+                    {/* <Link to= {{ pathname: '/', query: {closeChat: true, closeQuestion: false, problem: this.state.problem, tableTitle: this.state.tableTitle}}}><Button variant="outlined" color="primary">My question was not answered but I want to talk to a new tutor</Button></Link> */}
 
             </DialogContent>
             <DialogActions>
--- Toober/src/Components/HomePage.js
+++ Toober/src/Components/HomePage.js
@@ -1,26 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Theme from './Theme.js';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import Grid from '@material-ui/core/Grid';
 import FancyHomeButtons from'./Subcomponents/FancyHomeButtons';
+import firebase from 'firebase';
+import { REFUSED } from 'dns';
 
 
-const HomePage = () => {
-  return (
-     <div>
-     <MuiThemeProvider theme={Theme}>
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      closeChat: false,
+      closeQuestion: false,
+      problem: "",
+      tableTitle: "",
+      tuteeID: "",
+      tutorID: ""
+    }
+    if(props.location.query) {
+      console.log(props.location.query);
+      this.state = {
+        closeChat: props.location.query.closeChat,
+        closeQuestion: props.location.query.closeQuestion,
+        problem: props.location.query.problem,
+        tableTitle: props.location.query.tableTitle,
+        tuteeID: props.location.query.tuteeID,
+        tutorID: props.location.query.tutorID
+      }
+    }
+    this.closeChat = this.closeChat.bind(this);
+
+  };
+  
+  componentDidMount() {
+    console.log(this.state.tableTitle);
+    if(this.state.closeChat) {
+      this.closeChat();
+      if(this.state.closeQuestion) {
+        firebase.database().ref('problems/' + this.state.problem).remove();
+      }
+    }
+
+
+    this.setState({
+      closeChat: false,
+      closeQuestion: false
+    })
+  }
+
+  closeChat(ref) {
+    firebase.database().ref('chat/' + this.state.tableTitle + "/problem").remove();
+    firebase.database().ref('chat/' + this.state.tableTitle + "/tutorUID").remove();
+    firebase.database().ref('chat/' + this.state.tableTitle + "/tuteeUID").remove();
+    firebase.database().ref('chat/' + this.state.tableTitle).remove();
+  }
+
+  render() {
+    return (
       <div>
-      <Grid container spacing={3} justify="center"  direction="row" >
-        <Grid item >
-            <h1>Please select your role:</h1>
-        </ Grid>
-        </ Grid>
-    <FancyHomeButtons />
+      <MuiThemeProvider theme={Theme}>
+        <div>
+        <Grid container spacing={3} justify="center"  direction="row" >
+          <Grid item >
+              <h1>Please select your role:</h1>
+          </ Grid>
+          </ Grid>
+      <FancyHomeButtons />
+        </div>
+      </MuiThemeProvider>
       </div>
-    </MuiThemeProvider>
-     </div>
-  );
+    );
+  }
 }
  
 export default HomePage;

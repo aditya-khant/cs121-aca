3d63a2c31d1a8a27f3975169b2ac231566307cdc
Arielle Isaacs <amisaacs@g.hmc.edu>
2019-11-23 09:57:55 -0800

    bug when trying to render all the times on the public profile page

--- Toober/src/Components/Profiles/PublicProfile.js
+++ Toober/src/Components/Profiles/PublicProfile.js
@@ -9,14 +9,13 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 class Profile extends Component {
     constructor(props) {
     super(props);
-    console.log(props);
 
     this.state = {
         username: props.location.query.tutorName,
         uid: props.location.query.tutorUID,
-        isLoading: true,
-        time: 0
-    }
+        listOfTime: [],
+        isLoading: true
+      }
 
     }
     
@@ -25,27 +24,50 @@ class Profile extends Component {
     }
 
     async getTime(){
-      const tutorTime = await retrieve("users", this.state.uid, "tutorTime");
+      const userRef = firebase.database().ref("users/" + this.state.uid);
+      let times = []
+      userRef.once("value").then(function(snapshot) {
+        let data = snapshot.val();
+
+        for (let id in data) {
+          times.push({
+            id: id,
+            time: data[id]
+          })
+        }
+      })
       this.setState({
-        time: tutorTime,
-        isLoading: false
+        listOfTime: times,
+        isLoading:false
       })
     }
 
     render() {
-
-      let theTime;
-
-      theTime = `This tutor has spent a total of ${this.state.time} minutes tutoring.` ;
-
-        var user = firebase.auth().currentUser;
+      let times = this.state.listOfTime;
+      let list;
+      if (this.state.isLoading){
+        list = (
+          <CircularProgress />
+        );
+      }else {
+      list =  (
+          <List>
+            {times.map((content) => {
+              return(
+                <p>{content.time}</p>
+              )
+            })}
+            </List>
+        )
+      }
+      // theTime = `This tutor has spent a total of ${this.state.time} minutes tutoring.` ;
         let header;
 
-                header = (
-                  <div>
-                    <h1>{this.state.username}</h1>
-                  </div>
-                 )
+        header = (
+          <div>
+            <h1>{this.state.username}</h1>
+          </div>
+          )
 
         return (
 
@@ -55,7 +77,7 @@ class Profile extends Component {
           {header}
         </Grid>
         <Grid item xs={9}>
-          <h4>{theTime}</h4>
+          <h4>{list}</h4>
         </Grid>
         
         </MuiThemeProvider>
